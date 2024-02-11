1a727aabeb5531ad40a150783bdbb0a4136686e4
agustinmoreno22 <156142893+agustinmoreno22@users.noreply.github.com>
2024-02-11 18:16:00 -0300

    Add files via upload
    Bueno, veremos que pasa jaja. Aca metiendole onda

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var menu = document.querySelector('.menu');
+    menu.classList.toggle('show');
+}
+
+function closeNavbar() {
+    var menu = document.querySelector('.menu');
+    menu.classList.remove('show');
+}
